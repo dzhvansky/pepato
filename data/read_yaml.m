933fad9f1aa52808f8823723e8a5a66035efcfbb
dzhvansky <d.zhvansky@gmail.com>
2020-07-18 21:14:01 +0300

    Updated read_yaml

--- data/read_yaml.m
+++ data/read_yaml.m
@@ -1,46 +1,32 @@
-function results = read_yaml(filePath)
-% Lloyd Russell 2017
-% Simple little function to read simple little YAML format parameter files
+function results = read_yaml(file_path)
 
-% read file line by line
-fid = fopen(filePath, 'r');
+fid = fopen(file_path, 'r');
 data = textscan(fid, '%s', 'delimiter', '\n', 'whitespace', '');
 fclose(fid);
 
-% remove empty lines
 data = deblank(data{1});
 data(cellfun('isempty', data)) = [];
 
-% prepare final results structure
 results = [];
 
-% parse the contents (line by line)
 for i = 1:numel(data)
+    line = data{i};
     
-    % extract this line
-    thisLine = data{i};
-    
-    % ignore if this line is a comment
-    if strcmpi(thisLine(1), '#')
+    if strcmp(line(1), '#')
         continue
     end
     
-    % find the seperator between key and value
-    sepIndex = find(thisLine==':', 1, 'first');
+    sep_idx = find(line==':', 1, 'first');
     
-    % get the key name (remove whitespace)
-    key = strtrim(thisLine(1:sepIndex-1));  
+    key = strtrim(line(1:sep_idx-1));  
     
-    % get the value, ignoring any comments (remove whitespace)
-    value = strsplit(thisLine(sepIndex+1:end), '#');
+    value = strsplit(line(sep_idx+1:end), '#');
     value = strtrim(value{1}); 
     
-    % attempt to convert value to numeric type
-    [convertedValue, success] = str2num(value);
-    if success
-        value = convertedValue;
+    [numerical, status] = str2num(value);
+    if status
+        value = numerical;
     end
     
-    % store the key and value in the results
     results.(key) = value;
 end
