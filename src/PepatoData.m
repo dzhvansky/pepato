96418812fc8a8945db67c3112ae8ed1d632c5ac5
dzhvansky <d.zhvansky@gmail.com>
2020-12-15 17:26:11 +0300

    Updated format for PI output yaml files

--- src/PepatoData.m
+++ src/PepatoData.m
@@ -388,9 +388,12 @@ classdef PepatoData
             if nargin < 4 || ~strcmp(write_mode, {'single', 'multiple'})
                 write_mode = 'single';
             end
-            data_types = cell2struct({'vector', 'vector', 'vector_of_vector', 'vector_of_vector', 'vector_of_matricies', 'vector_of_matricies', 'vector_of_vector', ...
+            alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'};
+            data_types = cell2struct({'vector', 'vector', 'vector_of_vector', 'vector_of_vector', 'vector_of_matrix', 'vector_of_matricies', 'vector_of_vector', ...
                 'matrix', 'matrix', 'vector', 'matrix'}, ...
                 obj.output_params, 2);
+            col_labels = cell2struct({{'sacral', 'lumbar'}, {'sacral', 'lumbar'}, {'sacral', 'lumbar'}}, ...
+                {'motor_pool_max_activation', 'motor_pool_fwhm', 'motor_pool_similarity'}, 2);
             
             [subjects, trials, conditions] = get_trial_info(obj.filenames);
             assert(length(unique(subjects))==1)
@@ -407,7 +410,7 @@ classdef PepatoData
                     switch write_mode
                         case 'single'
                             fname_postfix = 'output';
-                            prefix = '    ';
+                            prefix = '  ';
                         case 'multiple'
                             fname_postfix = param_name;
                             prefix = '';
@@ -434,7 +437,7 @@ classdef PepatoData
                                     param_output{1, j} = num2str(param_value);
                                 case {'matrix', 'vector_of_vector'}
                                     param_output{1, j} = sprintf('[%s]', strjoin(num2str2cell(param_value), ', '));
-                                case 'vector_of_matricies'
+                                case 'vector_of_matrix'
                                     matrix_rows = {};
                                     for k = 1:size(param_value, 1)
                                         matrix_rows = [matrix_rows, sprintf('[%s]', strjoin(num2str2cell(param_value(k, :)), ', '))];
@@ -448,14 +451,40 @@ classdef PepatoData
                                     param_output{1, j} = 'NaN';
                                 case {'matrix', 'vector_of_vector'}
                                     param_output{1, j} = '[NaN]';
-                                case 'vector_of_matricies'
+                                case 'vector_of_matrix'
                                     param_output{1, j} = '[[NaN]]';
                             end
                         end
                     end
                     
                     fprintf(fout, [prefix 'type: %s\n'], param_type);
-                    fprintf(fout, [prefix 'value: [%s]\n'], strjoin(param_output, ', '));
+                    switch param_type
+                        case 'vector'
+                            fprintf(fout, [prefix 'label: [%s]\n'], strjoin(conditions, ', '));
+                            fprintf(fout, [prefix 'value: [%s]\n'], strjoin(param_output, ', '));
+                        case 'matrix'
+                            fprintf(fout, [prefix 'row_label: [%s]\n'], strjoin(conditions, ', '));
+                            fprintf(fout, [prefix 'col_label: [%s]\n'], strjoin(col_labels.(param_name), ', '));
+                            fprintf(fout, [prefix 'value: [%s]\n'], strjoin(param_output, ', '));
+                        case 'vector_of_vector'
+                            fprintf(fout, [prefix 'label: [%s]\n'], strjoin(conditions, ', '));
+                            fprintf(fout, [prefix 'value:\n']);
+                            for j = 1: n_conditions
+                                n_elements = length(strfind(param_output{1, j}, ', ')) + 1;
+                                fprintf(fout, [prefix '- label: [%s]\n'], strjoin(strcat('module_', alphabet(1:n_elements)), ', '));
+                                fprintf(fout, [prefix '  value: %s\n'], param_output{1, j});
+                            end
+                        case 'vector_of_matrix'
+                            fprintf(fout, [prefix 'label: [%s]\n'], strjoin(conditions, ', '));
+                            fprintf(fout, [prefix 'value:\n']);
+                            for j = 1: n_conditions
+                                n_rows = length(strfind(param_output{1, j}, '[')) - 1;
+                                n_cols = (length(strfind(param_output{1, j}, ', ')) + 1) / n_rows;
+                                fprintf(fout, [prefix '- raw_label: [%s]\n'], strjoin(strcat('module_', alphabet(1:n_rows)), ', '));
+                                fprintf(fout, [prefix '  col_label: [%s]\n'], strjoin(strcat('ref_module_', num2str2cell(1:n_cols)), ', '));
+                                fprintf(fout, [prefix '  value: %s\n'], param_output{1, j});
+                            end
+                    end
                     fclose(fout);
                 end
                 
